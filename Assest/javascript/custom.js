27a65671676634923d9fb477a638677d26b606ec
Priyanka Sharma <mohitkaushik5689@gmail.com>
2026-06-04 14:48:28 +0530

    Added new features

---
+++ Assest/javascript/custom.js
@@ -0,0 +1,15 @@
+
+
+
+function toggleMenu(){
+    var menu = document.getElementById("menu");
+
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
+
+
+
